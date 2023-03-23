d25cfcf8f2df16ff69a5f4ce3459d6a3525d21ec
Suhag <suhagmohinkar100@gmail.com>
2023-03-24 00:08:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let signupBtn = document.getElementById("signupBtn");
+let usersigninBtn = document.getElementById("tutersigninBtn");
+let tutorsigninBtn = document.getElementById("usersigninBtn");
+
+usersigninBtn.onclick = function
